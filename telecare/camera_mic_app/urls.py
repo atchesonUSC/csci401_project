518e7dc38320c5b28e1a6137a77d5840b7cb5056
Sarah Okamoto <sarahokamoto@Sarahs-MBP-2.lan>
2021-05-05 06:07:07 -0700

    added pages for cam_mic module and added templatae

--- telecare/camera_mic_app/urls.py
+++ telecare/camera_mic_app/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.cam_mic_user_status, name='cam_mic'),
-    path('system/', views.cam_mic_user_status1, name='cam_mic_system'),
-    path('ios/', views.cam_mic_user_status2, name='cam_mic_ios_help'),
-    path('android/', views.cam_mic_user_status3, name='cam_mic_android_help'),
-    path('new/<page>/', views.cam_mic_new, name='cam_mic_new'),
-    path('returning/<page>/', views.cam_mic_returning, name='cam_mic_returning'),
+    path('<page>/', views.cam_mic, name='cam_mic'),
+
+    # path('', views.cam_mic_user_status, name='cam_mic'),
+    # path('system/', views.cam_mic_user_status1, name='cam_mic_system'),
+    # path('ios/', views.cam_mic_user_status2, name='cam_mic_ios_help'),
+    # path('android/', views.cam_mic_user_status3, name='cam_mic_android_help'),
+    # path('new/<page>/', views.cam_mic_new, name='cam_mic_new'),
+    # path('returning/<page>/', views.cam_mic_returning, name='cam_mic_returning'),
 ]
--- telecare/camera_mic_app/views.py
+++ telecare/camera_mic_app/views.py
@@ -1,33 +1,37 @@
 from django.shortcuts import render
 
-
-def cam_mic_user_status(request):
-    page = 'Cam_Mic_0.html'
+def cam_mic(request, page):
+    page = 'cam_mic_' + page + '.html'
     args = {}
     return render(request, page, args)
 
-def cam_mic_user_status1(request):
-    page = 'Cam_Mic_1.html'
-    args = {}
-    return render(request, page, args)
+# def cam_mic_user_status(request):
+#     page = 'Cam_Mic_0.html'
+#     args = {}
+#     return render(request, page, args)
+
+# def cam_mic_user_status1(request):
+#     page = 'Cam_Mic_1.html'
+#     args = {}
+#     return render(request, page, args)
     
-def cam_mic_user_status2(request):
-    page = 'Cam_Mic_2.html'
-    args = {}
-    return render(request, page, args)
+# def cam_mic_user_status2(request):
+#     page = 'Cam_Mic_2.html'
+#     args = {}
+#     return render(request, page, args)
     
-def cam_mic_user_status3(request):
-    page = 'Cam_Mic_3.html'
-    args = {}
-    return render(request, page, args)
+# def cam_mic_user_status3(request):
+#     page = 'Cam_Mic_3.html'
+#     args = {}
+#     return render(request, page, args)
 
-def cam_mic_new(request):
-    page = 'cam_mic_new_' + page + '.html'
-    args = {}
-    return render(request, page, args)
+# def cam_mic_new(request):
+#     page = 'cam_mic_new_' + page + '.html'
+#     args = {}
+#     return render(request, page, args)
 
 
-def cam_mic_returning(request, page):
-    page = 'cam_mic_returning_' + page + '.html'
-    args = {}
-    return render(request, page, args)
+# def cam_mic_returning(request, page):
+#     page = 'cam_mic_returning_' + page + '.html'
+#     args = {}
+#     return render(request, page, args)
